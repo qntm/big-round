f8cf520a53df3c738049c58748333fccd927a1a5
qntm <qntm@users.noreply.github.com>
2020-06-04 10:56:36 +0100

    despite your directions

--- src/index.js
+++ src/index.js
@@ -11,81 +11,47 @@ const ROUNDING_MODE = {
   NEAREST_HALF_TO_ODD: 9
 }
 
-const TRUNCATED = {
-  NOTHING: 0,
-  LESS_THAN_POINT_FIVE: 1,
-  POINT_FIVE: 2,
-  MORE_THAN_POINT_FIVE: 3
-}
-
-const SIGN = {
-  NEGATIVE: 0,
-  ZERO: 1,
-  POSITIVE: 2
-}
-
 const divide = (roundingMode, a, b) => {
   const q = a / b
 
-  // The true quotient (call it QR) is rounded towards zero by default. We may
-  // need to undo this, by adding back 1n, or -1n if QR was negative.
+  // The real quotient (call it `r`) was rounded towards zero to yield `q`.
+  // We may need to undo this, by adding back 1n, or -1n if `r` was negative.
 
-  // What was the sign of QR?
-  let sign
-  if (a === 0n) {
-    sign = SIGN.ZERO
-  } else {
-    sign = a > 0n === b > 0n ? SIGN.POSITIVE : SIGN.NEGATIVE
-  }
+  // What was the sign of `r`? May be non-zero even if `q` is `0n` e.g. 1n / 10n
+  const sign = a === 0n ? 0n
+    : a > 0n === b > 0n ? 1n
+    : -1n
 
-  // What fraction was truncated to make QR into `q`?
-  const absA = a > 0n ? a : -a
-  const absB = b > 0n ? b : -b
+  // What fraction was truncated to make `r` into `q`?
+  const absA = a >= 0n ? a : -a
+  const absB = b >= 0n ? b : -b
 
-  let truncated
   const twiceRemainder = absA % absB * 2n
-  if (twiceRemainder === 0n) {
-    truncated = TRUNCATED.NOTHING
-  } else if (twiceRemainder < absB) {
-    truncated = TRUNCATED.LESS_THAN_POINT_FIVE
-  } else if (twiceRemainder === absB) {
-    truncated = TRUNCATED.POINT_FIVE
-  } else {
-    // Maximum possible value is `2n * absB - 1n`
-    truncated = TRUNCATED.MORE_THAN_POINT_FIVE
-  }
 
-  let bump
-  if (roundingMode === ROUNDING_MODE.DIRECTED_TOWARDS_ZERO) {
-    bump = false
-  } else if (roundingMode === ROUNDING_MODE.DIRECTED_AWAY_FROM_ZERO) {
-    bump = truncated !== TRUNCATED.NOTHING
-  } else if (roundingMode === ROUNDING_MODE.DIRECTED_TOWARDS_NEGATIVE_INFINITY) {
-    bump = truncated !== TRUNCATED.NOTHING && sign === SIGN.NEGATIVE
-  } else if (roundingMode === ROUNDING_MODE.DIRECTED_TOWARDS_POSITIVE_INFINITY) {
-    bump = truncated !== TRUNCATED.NOTHING && sign === SIGN.POSITIVE
-  } else if (roundingMode === ROUNDING_MODE.NEAREST_HALF_TOWARDS_ZERO) {
-    bump = truncated === TRUNCATED.MORE_THAN_POINT_FIVE
-  } else if (roundingMode === ROUNDING_MODE.NEAREST_HALF_AWAY_FROM_ZERO) {
-    bump = truncated === TRUNCATED.MORE_THAN_POINT_FIVE ||
-      truncated === TRUNCATED.POINT_FIVE
-  } else if (roundingMode === ROUNDING_MODE.NEAREST_HALF_TOWARDS_NEGATIVE_INFINITY) {
-    bump = truncated === TRUNCATED.MORE_THAN_POINT_FIVE ||
-      (truncated === TRUNCATED.POINT_FIVE && sign === SIGN.NEGATIVE)
-  } else if (roundingMode === ROUNDING_MODE.NEAREST_HALF_TOWARDS_POSITIVE_INFINITY) {
-    bump = truncated === TRUNCATED.MORE_THAN_POINT_FIVE ||
-      (truncated === TRUNCATED.POINT_FIVE && sign === SIGN.POSITIVE)
-  } else if (roundingMode === ROUNDING_MODE.NEAREST_HALF_TO_EVEN) {
-    bump = truncated === TRUNCATED.MORE_THAN_POINT_FIVE ||
-      (truncated === TRUNCATED.POINT_FIVE && q % 2n !== 0n)
-  } else if (roundingMode === ROUNDING_MODE.NEAREST_HALF_TO_ODD) {
-    bump = truncated === TRUNCATED.MORE_THAN_POINT_FIVE ||
-      (truncated === TRUNCATED.POINT_FIVE && q % 2n === 0n)
-  } else {
+  const undoTruncation = twiceRemainder === 0n ? false // no rounding occurred
+    : roundingMode === ROUNDING_MODE.DIRECTED_TOWARDS_ZERO ? false
+    : roundingMode === ROUNDING_MODE.DIRECTED_AWAY_FROM_ZERO ? true
+    : roundingMode === ROUNDING_MODE.DIRECTED_TOWARDS_NEGATIVE_INFINITY ? sign < 0n
+    : roundingMode === ROUNDING_MODE.DIRECTED_TOWARDS_POSITIVE_INFINITY ? sign > 0n
+
+    // All modes beyond this point round to nearest
+    : twiceRemainder > absB ? true // rounded by more than .5, undo that
+    : twiceRemainder < absB ? false // rounded by less than .5, leave it
+
+    // Rounded by .5 exactly, decide tie break
+    : roundingMode === ROUNDING_MODE.NEAREST_HALF_TOWARDS_ZERO ? false
+    : roundingMode === ROUNDING_MODE.NEAREST_HALF_AWAY_FROM_ZERO ? true
+    : roundingMode === ROUNDING_MODE.NEAREST_HALF_TOWARDS_NEGATIVE_INFINITY ? sign < 0n
+    : roundingMode === ROUNDING_MODE.NEAREST_HALF_TOWARDS_POSITIVE_INFINITY ? sign > 0n
+    : roundingMode === ROUNDING_MODE.NEAREST_HALF_TO_EVEN ? q % 2n !== 0n
+    : roundingMode === ROUNDING_MODE.NEAREST_HALF_TO_ODD ? q % 2n === 0n
+    : undefined
+
+  if (undoTruncation === undefined) {
     throw Error('Unrecognised rounding strategy')
   }
 
-  return q + (bump ? (sign === SIGN.POSITIVE ? 1n : -1n) : 0n)
+  return q + (undoTruncation ? sign : 0n)
 }
 
 module.exports = {
